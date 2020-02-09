0846a26825927a00e014dc0e652da4aef0e9101c
Iqbal Fauzi <iqbalfawz@gmail.com>
2020-02-09 20:18:50 +0700

    First commit

---
+++ modules/admin-event-venue/config.php
@@ -0,0 +1,63 @@
+<?php
+
+return [
+    '__name' => 'admin-event-venue',
+    '__version' => '0.0.1',
+    '__git' => 'git@github.com:getmim/admin-event-venue.git',
+    '__license' => 'MIT',
+    '__author' => [
+        'name' => 'Iqbal Fauzi',
+        'email' => 'iqbalfawz@gmail.com',
+        'website' => 'http://iqbalfn.com/'
+    ],
+    '__files' => [
+        'modules/admin-event-venue' => ['install','update','remove']
+    ],
+    '__dependencies' => [
+        'required' => [
+            [
+                'admin' => NULL
+            ],
+            [
+                'admin-event' => NULL
+            ],
+            [
+                'admin-venue' => NULL
+            ],
+            [
+                'lib-form' => NULL
+            ],
+            [
+                'lib-formatter' => NULL
+            ]
+        ],
+        'optional' => []
+    ],
+    'autoload' => [
+        'classes' => [],
+        'files' => []
+    ],
+    'libForm' => [
+        'forms' => [
+            'admin.event.edit' => [
+                'organizer' => [
+                    'label' => 'Event',
+                    'type' => 'select',
+                    'sl-filter' => [
+                        'route' => 'adminObjectFilter',
+                        'params' => [],
+                        'query' => [
+                            'type' => 'venue'
+                        ]
+                    ],
+                    'rules' => [
+                        'exists' => [
+                            'model' => 'Venue\\Model\\Venue',
+                            'field' => 'id'
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ]
+];
